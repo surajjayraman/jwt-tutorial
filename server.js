16172536a90465ef71da780b6d61c89f2a7a5deb
surajjayraman <suraj.jayraman@gmail.com>
2022-04-29 19:04:31 -0400

    Add authentication server for token refresh.

--- server.js
+++ server.js
@@ -14,26 +14,17 @@ const posts = [
     {
         username: "Nair",
         title: "Post 2"
+    },
+    {
+        username: "Awesome",
+        title: "Awesome Day!"
     }
 
 ]
 
 // return the posts
-app.get('/posts', authenticateToken, (req,res) => {
-    
+app.get('/posts', authenticateToken, (req,res) => {    
     res.json(posts.filter(post => post.username === req.user.name));
-})
-
-app.post('/login', (req, res) => {
-    // Authenticate User
-    // serialize user object with JWT using secret key
-    const username = req.body.username;  
-    const user = { name: username};
-
-    //require('crypto').randomBytes(64).toString('hex')
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-    res.json({ accessToken: accessToken})
-
 })
 
 function authenticateToken(req, res, next){
