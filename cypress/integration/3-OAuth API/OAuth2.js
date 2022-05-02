208047626c0650eb9c13a6349fdf122be271b4a9
surajjayraman <suraj.jayraman@gmail.com>
2022-05-02 15:52:10 -0400

    Add Cypress tests for Refresh JWT.

--- cypress/integration/3-OAuth API/OAuth2.js
+++ cypress/integration/3-OAuth API/OAuth2.js
@@ -2,31 +2,73 @@
 
 describe('OAuth feature APIs', () => {
 
-    let accessToken = '';  
+  let accessToken = '';
+  let refreshToken = '';
 
-    it('get the access token test', ()=>{
+  it('get the access token test', ()=>{
 
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:4000/login',
+      body: {
+        "username": "Suraj"
+      }
+    }).then(response => {
+      cy.log(JSON.stringify(response));
+      cy.log(response.body.accessToken);
+      accessToken = response.body.accessToken;
+      refreshToken = response.body.refreshToken;
+
+      cy.request({
+        method: 'GET',
+        url: 'http://localhost:3000/posts',
+        headers: {
+          'Authorization' : 'Bearer ' + accessToken
+
+        }
+      }).then(response => {
+        cy.log(JSON.stringify(response));
+      });
+    });
+  });
+
+
+  it('cy.wait() - wait for a specific amount of time', () => {
+    cy.wait(1500);
+    cy.request({
+      method: 'GET',
+      url: 'http://localhost:3000/posts',
+      headers: {
+        'Authorization' : 'Bearer ' + accessToken
+
+      }
+    }).then(response => {
+      cy.log(JSON.stringify(response));
+      cy.wait(15000);
+      cy.request({
+        method: 'POST',
+        url: 'http://localhost:4000/token',
+        body: {
+          "token": refreshToken
+        }
+      }).then(response => {
+        cy.log(JSON.stringify(response));
+        //cy.log(response.body.accessToken);
+        accessToken = response.body.accessToken;
         cy.request({
-            method: 'POST',
-            url: 'http://localhost:4000/login',
-            body: {
-                "username": "Suraj"
+            method: 'GET',
+            url: 'http://localhost:3000/posts',
+            headers: {
+              'Authorization' : 'Bearer ' + accessToken
+    
             }
-        }).then(response => {
+          }).then(response => {
             cy.log(JSON.stringify(response));
-            cy.log(response.body.accessToken);
-            accessToken = response.body.accessToken;
+          });
+        cy.wait(15000);
+      })
 
-            cy.request({
-                method: 'GET',
-                url: 'http://localhost:3000/posts',
-                headers: {
-                    'Authorization' : 'Bearer ' + accessToken
-
-                }
-            }).then( response => {
-                cy.log(JSON.stringify(response));                
-            })
-        })
-    })
-})
+    });
+        
+  });
+});
